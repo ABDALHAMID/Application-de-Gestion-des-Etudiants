8b9e082c70eb9af5b41191c7152ba07a9014470f
ABDALHAMID <ABD.VOU@GMAIL.COM>
2025-01-30 01:19:40 +0100

    change the header and add routing

--- Angular-frontend/src/app/app.routes.ts
+++ Angular-frontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { CreateStudentComponent } from './create-student/create-student.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:StudentsListComponent},
+  {path: 'createStudent', component:CreateStudentComponent}
+];
--- Angular-frontend/src/app/content-body/content-body.component.ts
+++ Angular-frontend/src/app/content-body/content-body.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { StudentsListComponent } from "../students-list/students-list.component";
 import { SideBarComponent } from "../side-bar/side-bar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-content-body',
   standalone: true,
-  imports: [StudentsListComponent, SideBarComponent],
+  imports: [StudentsListComponent, SideBarComponent, RouterOutlet],
   templateUrl: './content-body.component.html',
-  styleUrl: './content-body.component.scss'
+  styleUrl: './content-body.component.scss',
 })
 export class ContentBodyComponent {
 
