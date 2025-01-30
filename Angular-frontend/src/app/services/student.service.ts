9a145f515afb2f7fea07d913823d406a7d30e188
ABDALHAMID <ABD.VOU@GMAIL.COM>
2025-01-30 16:31:54 +0100

    add the student service

---
+++ Angular-frontend/src/app/services/student.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Student } from '../interfaces/student';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentService {
+
+  private backendApi = "http://localhost:3000";
+  private subApi = "students"
+  private apiUrl =  `${this.backendApi}/${this.subApi}`;
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllStudents(): Observable<Student[]>{
+    return this.httpClient.get<Student[]>(this.apiUrl)
+  }
+
+  getStudent(id: number): Observable<Student>{
+    return this.httpClient.get<Student>(`${this.apiUrl}/${id}`)
+  }
+
+  createStudent(student: Student): Observable<Student>{
+    return this.httpClient.post<Student>(`${this.apiUrl}`, student)
+  }
+
+  updateStudent(student: Student): Observable<Student>{
+    return this.httpClient.put<Student>(`${this.apiUrl}/${student.id}`, student)
+  }
+
+  deleteStudent(id: number): Observable<Student>{
+    return this.httpClient.delete<Student>(`${this.apiUrl}/${id}`)
+  }
+
+
+
+}
